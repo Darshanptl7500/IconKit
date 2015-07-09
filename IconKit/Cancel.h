2c3519fb302c821c8c1dc895909d420cc00555a4
Darshan Patel <darshanptl89@gmail.com>
2015-07-09 18:36:11 +0530

    Add Cancel Class

---
+++ IconKit/Cancel.h
@@ -0,0 +1,13 @@
+//
+//  Cancel.h
+//  IconKit
+//
+//  Created by IQR IT on 7/9/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cancel : NSObject
+
+@end
---
+++ IconKit/Cancel.m
@@ -0,0 +1,13 @@
+//
+//  Cancel.m
+//  IconKit
+//
+//  Created by IQR IT on 7/9/15.
+//
+//
+
+#import "Cancel.h"
+
+@implementation Cancel
+
+@end
